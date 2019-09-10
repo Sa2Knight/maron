063ff1c78ff3d29192e4d933862383c1c85cdc3a
Shingo sasaki <shingo.sasaki.0529@gmail.com>
2019-09-10 16:52:59 +0000

    Let文の識別子までparseできるように

--- parser/parser.go
+++ parser/parser.go
@@ -22,9 +22,68 @@ func New(l *lexer.Lexer) *Parser {
 	return p
 }
 
-// ParseProgram ソースコードのパースを行い、ASTを生成して戻す
+// ParseProgram ソースコードのパースを行い、ASTのルートノード生成して戻す
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	// let
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	// 変数名
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	// =
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// 式(の予定)
+	// TODO: セミコロンに遭遇するまで式を読み飛ばしてしまっている
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+	return false
 }
 
 func (p *Parser) nextToken() {
